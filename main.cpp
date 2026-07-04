8261c9863d5b97b40486b1034d532aa7613bad4d
Charan Ram Sai <charankotta32@gmail.com>
2026-07-04 21:09:17 +0530

    Refactor: Removed tutorial comments to enforce Clean Code principles

--- main.cpp
+++ main.cpp
@@ -3,27 +3,22 @@
 
 using namespace std;
 
-// 1. THE BASE CLASS (The Master Blueprint)
 class Sensor {
 public:
     string sensorName;
 
-    // 'virtual' is a magic keyword. It means "I expect my children to change this function"
     virtual void scanEnvironment() {
         cout << "[Generic Sensor] Scanning..." << endl;
     }
 };
 
-// 2. THE CHILD CLASS (Inherits from Sensor)
 class mmWaveRadar : public Sensor {
 public:
-    // We 'override' the master function to do exactly what the Radar needs
     void scanEnvironment() override {
         cout << "[" << sensorName << "] PENETRATING CONCRETE... Human Breathing Detected at 4.2 Meters." << endl;
     }
 };
 
-// 3. ANOTHER CHILD CLASS
 class LoRaRadio : public Sensor {
 public:
     void scanEnvironment() override {
@@ -39,7 +34,7 @@ public:
 };
 
 int main() {
-    // Let's create our objects!
+
     mmWaveRadar radar;
     radar.sensorName = "HLK-LD2410";
 
@@ -49,7 +44,6 @@ int main() {
     AcousticMic acousticMicNode;
     acousticMicNode.sensorName = "Microphone";
 
-    // Execute the systems
     cout << "--- RESQ-SANJEEVINI BOOT SEQUENCE ---" << endl;
     radar.scanEnvironment();
     radioNode.scanEnvironment();
